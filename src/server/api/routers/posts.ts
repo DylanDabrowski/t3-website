04d40d981a3728fd0bac78cb3cb77038c0a4e8ae
DylanDabrowski <dylandabrowski@gmail.com>
2023-03-30 22:50:09 -0400

    vercel, planetscale, and other stuff all setup, starting adding assets

---
+++ src/server/api/routers/posts.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { TRPCError } from "@trpc/server";
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
+
+export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany();
+  }),
+
+  create: publicProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        content: z.string()?.optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const post = await ctx.prisma.post.create({
+        data: {
+          title: input.title,
+          description: input.description,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
+});
